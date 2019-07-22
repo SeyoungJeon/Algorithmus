bb055a3f16304264f88dd6cf1f9c2b39b6cccac9
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-22 19:20:19 +0900

    백준 10952번 문제 품

--- Algorithmus/Algorithmus/10950.cpp
+++ Algorithmus/Algorithmus/10950.cpp
@@ -21,3 +21,20 @@ int main() {
 
 	return 0;
 }
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	int A, B;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+
+	return 0;
+}
