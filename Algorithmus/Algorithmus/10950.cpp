797c9140bab6d8ba2dde2263c403e52428032403
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-22 17:53:17 +0900

    백준 10950번 문제 품

---
+++ Algorithmus/Algorithmus/10950.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	int A, B;
+	cin >> T;
+	int *result = new int[T];
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		result[i] = A + B;
+	}
+
+	for (int i = 0; i < T; i++) {
+		cout << result[i] << endl;
+	}
+
+	delete[] result;
+
+	return 0;
+}
