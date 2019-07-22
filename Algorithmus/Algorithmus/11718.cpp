877693b880aa065bf038665d6e50b47bd5b03081
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-22 20:36:10 +0900

    백준 11719번 품

---
+++ Algorithmus/Algorithmus/11718.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string input;
+	while (true) {
+		getline(cin, input);
+		if (input == "") 
+			break;
+		cout << input << endl;
+	}
+	return 0;
+}
