8299a089b8c3ef7e14954d0d0e489dab8453a9a7
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-25 07:20:11 +0900

    백준 10844번 보류

--- Algorithmus/Algorithmus/10844.cpp
+++ Algorithmus/Algorithmus/10844.cpp
@@ -2,18 +2,30 @@
 
 using namespace std;
 
-//int recursive(int *arr) {
-//
-//}
-
 int main() {
+	int arr[101][11];
 	int N;
 	cin >> N;
-	int arr[101];
-	for (int i = 0; i < 101; i++) {
-		arr[i] = i;
-		cout << arr[i] << '\n';
+	for (int i = 1; i < 10; i++) {
+		arr[1][i] = 1;
+	}
+	arr[1][0] = 0;
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == 0)
+				arr[i][j] = arr[i - 1][j + 1];
+			else if (j == 9)
+				arr[i][j] = arr[i - 1][j - 1];
+			else
+				arr[i][j] = (arr[i - 1][j - 1] + arr[i - 1][j + 1]) % 1000000000;
+		}
 	}
 
+	int total = 0;
+	for (int i = 0; i < 10; i++) {
+		total += arr[N][i];
+	}
+	cout << total;
 	return 0;
 }
