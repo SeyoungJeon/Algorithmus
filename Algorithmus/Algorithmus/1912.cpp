882d58cc85f2abdc8dc18a36ea1ca3bbe628f5ae
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-26 16:25:04 +0900

    백준 1912번 다른 사람의 답과 논리가 같은데 내 답만 틀림 질문 올려놓음

---
+++ Algorithmus/Algorithmus/1912.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <limits.h>
+using namespace std;
+
+int dp[100001], a[100001], n,s;
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		dp[i] = a[i];
+		if (a[i] + dp[i-1] > a[i]) {
+			dp[i] = a[i] + dp[i - 1];
+		}
+		s = max(s, dp[i]);
+	}
+
+	cout << s;
+	return 0;
+}
+
