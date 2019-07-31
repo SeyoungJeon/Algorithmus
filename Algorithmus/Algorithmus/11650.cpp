4a7ab260f3f18f1f5c132151907899ff65a462f0
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-31 17:50:58 +0900

    백준 11650번 품

---
+++ Algorithmus/Algorithmus/11650.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+// sort 함수에 사용할 비교함수 선언
+bool compare(pair<int, int> &a, pair<int, int> &b) {
+	//X 좌표 비교
+	if (b.first > a.first) {
+		return true;
+	}
+	else if (a.first == b.first) {
+		// X좌표 같을 때 Y 좌표 비교
+		if (b.second > a.second) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else {
+		return false;
+	}
+}
+
+int main() {
+	int N;
+	vector<pair<int, int>> v;
+	scanf("%d", &N);
+	v.resize(N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &v[i].first, &v[i].second);
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+
+	return 0;
+}
