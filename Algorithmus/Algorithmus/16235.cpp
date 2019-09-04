19438cabc6e860dc8500a11cfe8a38257ac12869
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-09-05 03:36:44 +0900

    백준 (삼성 기출) 16235번 품

--- Algorithmus/Algorithmus/16235.cpp
+++ Algorithmus/Algorithmus/16235.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
+#include <deque>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
 
 struct tree {
-	int y, x;
 	int age;
 	bool live;
 };
 
-vector<tree> v;
-int N, M, K, **map,**temp,ans = 0;
+vector<tree> **v;
+int N, M, K, **map, **temp, ans = 0;
 
 int dx[8] = { -1, 1, 0, 0 , 1,-1 , -1, 1 };
 int dy[8] = { 0, 0 ,-1, 1 , 1, 1 , -1 ,-1 };
 
 
-bool compare(const tree &a ,const tree &b) {
+bool compare(const tree &a, const tree &b) {
 	if (a.age < b.age) {
 		return true;
 	}
@@ -33,9 +33,11 @@ int main() {
 	cin >> N >> M >> K;
 	map = new int*[N];
 	temp = new int*[N];
-	for (int i = 0; i < N; i++){
+	v = new vector<tree>*[N];
+	for (int i = 0; i < N; i++) {
 		map[i] = new int[N];
 		temp[i] = new int[N];
+		v[i] = new vector<tree>[N];
 	}
 
 	for (int i = 0; i < N; i++) {
@@ -48,55 +50,68 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		int y, x, z;
 		cin >> y >> x >> z;
-		v.push_back({ y - 1,x - 1,z,true });
-		
+		v[y - 1][x - 1].push_back({ z,true });
 	}
 
-	int cnt = 0;
-	while (cnt < K) {
+	while (K-- > 0) {
 
-		sort(v.begin(), v.end(), compare);
-		
 		// 봄
-		for (size_t i = 0; i < v.size(); i++) {
-			if (map[v[i].y][v[i].x] - v[i].age >= 0) {
-				map[v[i].y][v[i].x] -= v[i].age;
-				v[i].age += 1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sort(v[i][j].begin(), v[i][j].end(),compare);
+				for (auto k : v[i][j]) {
+					if (map[i][j] >= k.age) {
+						map[i][j] -= k.age;
+						k.age += 1;
+					}
+					else {
+						k.live = false;
+					}
+				}
 			}
-			else {
-				v[i].live = false;
-			} 
 		}
-		
+
 		//여름 
-		for (auto iter = v.begin(); iter != v.end();) {
-			if (!(*iter).live) {
-				map[(*iter).y][(*iter).x] += (*iter).age / 2;
-				iter = v.erase(iter);
-			}
-			else {
-				iter++;
-			}
-		}
-	
-		// 가을 나이 5배수일 경우 8방향에 나무 심기
-		size_t s = v.size();
-		for (size_t k = 0; k < s; k++) {
-			if (v[k].age % 5 == 0) {
-				for (int i = 0; i < 8; i++) {
-					int nx = v[k].x + dx[i];
-					int ny = v[k].y + dy[i];
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-						continue;
-
-					tree nt = { ny,nx,1,true};
-					v.push_back(nt);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (auto iter = v[i][j].begin(); iter != v[i][j].end();) {
+					if (!(*iter).live) {
+						map[i][j] += (*iter).age / 2;
+						iter = v[i][j].erase(iter);
+					}
+					else {
+						iter++;
+					}
 				}
 			}
 		}
 		
+
+		// 가을 나이 5배수일 경우 8방향에 나무 심기
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cout << i << ' ' << j << '\n';
+				size_t s = v[i][j].size();
+				for (size_t k = 0; k < s; k++) {
+					if (v[i][j][k].age % 5 == 0) {
+						for (int m = 0; m < 8; m++) {
+							int nx = j + dx[m];
+							int ny = i + dy[m];
+
+							cout << nx << ' ' << ny << '\n';
+
+							if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+								continue;
+
+							tree nt = { 1,true };
+							v[i][j].push_back(nt);
+						}
+					}
+				}
+			}
+		}
 		
+
 		//겨울 양분 추가
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
@@ -104,15 +119,14 @@ int main() {
 			}
 		}
 
-		cnt++;
 	}
 
-	for (auto k : v) {
-		if (k.live){
-			ans++;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			ans += v[i][j].size();
 		}
 	}
-
+	
 	cout << ans << '\n';
 
 	return 0;
